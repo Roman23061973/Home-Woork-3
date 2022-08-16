642d43ba2dc301cb258c7f4d25d8641f1e75072f
roman <ya.androm2014@yandex.ru>
2022-08-16 21:21:24 +0300

    task 2

---
+++ task 1/Program.cs
@@ -0,0 +1,20 @@
+﻿
+Console.WriteLine("Введите координаты точки А по х");
+double xA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты точки А по y");
+double yA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты точки А по z");
+double zA = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите коорлинаты точки В по x");
+double xB = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координаты точки В по y");
+double yB = Cjnvert.ToDouble(Console.ReadLine;
+Console.WriteLine("Введите координаты точки В по z"));
+double zB = Convert.ToDouble(Console.ReadLine());
+
+double res = Math.Sqrt(Math.Pow(xB - xA, 2)  + Math.Pow(yB - yA, 2) + Math.Pow(zB - za, 2));
+
+Console.WriteLine($"{res:F2}");
+
+
+
